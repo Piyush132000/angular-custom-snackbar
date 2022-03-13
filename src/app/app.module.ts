87dd9c234ba2be6c6a5b01a76def51307dfbc22a
Piyush132000 <piyush132000@gmail.com>
2022-03-13 11:34:05 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CustomSnackbarComponent } from './custom-snackbar/custom-snackbar.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar/snack-bar-module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MatSnackBarModule],
+  declarations: [AppComponent, HelloComponent, CustomSnackbarComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
